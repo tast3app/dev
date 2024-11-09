7ec4c7f622229eee6a2f2a77b10aa3c35c3a948b
alex <alex.scott@k-int.co.uk>
2024-11-09 12:46:51 +0000

    Updates

---
+++ precache-manifest.ded328584c2573063ce8e2befc933f3f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fc8c12a4c1b0ce2ee1188f1c7c15014",
+    "url": "/dev/index.html"
+  },
+  {
+    "revision": "ad7d68d6a9630d1303ce",
+    "url": "/dev/static/css/main.42fdcda1.chunk.css"
+  },
+  {
+    "revision": "2f234825e9d5c17609e4",
+    "url": "/dev/static/js/2.db18ff85.chunk.js"
+  },
+  {
+    "revision": "2c10491364e9bdf48063",
+    "url": "/dev/static/js/3.fc5cbf83.chunk.js"
+  },
+  {
+    "revision": "ad7d68d6a9630d1303ce",
+    "url": "/dev/static/js/main.f3aa1822.chunk.js"
+  },
+  {
+    "revision": "7d94ea1dc864dbdfcfc0",
+    "url": "/dev/static/js/runtime~main.bb967cf0.js"
+  },
+  {
+    "revision": "31cbe65e943a837de2b18dd97e0e2295",
+    "url": "/dev/static/media/home-background.31cbe65e.jpg"
+  },
+  {
+    "revision": "9811b06ef159bc163e865757898f018b",
+    "url": "/dev/static/media/spaghetti-bolognese.9811b06e.jpg"
+  }
+]);
